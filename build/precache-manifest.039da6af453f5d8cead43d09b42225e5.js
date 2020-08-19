8c60d26e757d6124ffa047fa8ae7440b3fc3bcce
NazarLiberty <NazarLiberty@gmail.com>
2020-08-19 15:03:08 +0300

    test build

---
+++ build/precache-manifest.039da6af453f5d8cead43d09b42225e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c28d16610123c5aacf4d2d399a7cfb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fe0c5c94b7a51f39b10e",
+    "url": "./static/css/main.c15ffc08.chunk.css"
+  },
+  {
+    "revision": "55fefce4619e62ca1510",
+    "url": "./static/js/2.5046b7be.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "./static/js/2.5046b7be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0c5c94b7a51f39b10e",
+    "url": "./static/js/main.81dad097.chunk.js"
+  },
+  {
+    "revision": "7e32f7237c028bed8ac9",
+    "url": "./static/js/runtime-main.c4d7a66e.js"
+  },
+  {
+    "revision": "a60a7214031ce453e40fda7f469dbeeb",
+    "url": "./static/media/mobile-background.a60a7214.jpg"
+  }
+]);
